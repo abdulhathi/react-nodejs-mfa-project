5a99d8e347c7203e351551fd91c78541e788a125
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2025-12-06 21:31:21 -0800

    app middleware and listener setup in index.ts

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { json, urlencoded } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import session from 'express-session'
+import passport from 'passport'
+
+dotenv.config()
+
+const app = express()
+// Middlewares
+app.use(json({ limit: '100mb' }))
+app.use(urlencoded({ limit: '100mb', extended: true }))
+app.use(
+  cors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+  })
+)
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 600,
+    },
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+// Routes
+
+// Listen app
+const port = process.env.PORT || 7001
+app.listen(port, () => console.log(`Server is running on port ${port}`))
